38b8f9c1259a4819cc77ba31aa6c8c10c08b8b54
Mahnoor-ismail01 <mahnoor@students.uit.edu>
2023-07-22 00:56:21 +0500

    new modifications  and parser for badelement

--- compare.py
+++ compare.py
@@ -1,65 +1,270 @@
 def extract_and_compare_deep_general(badnets):
-   
     for set_num, circuit_set in enumerate(badnets):
-        print(f"Processing circuit set {set_num + 1}...")
-
-
         for i in range(1, len(circuit_set)):
-            print(f"Circuit {i + 1} info: ", circuit_set[i])
+            print(f"\nCircuit {i + 1}:")
+            missing_nets, extra_nets, unknown_nets, device_diff = compare_circuits(circuit_set[0], circuit_set[i])
+            print_info(missing_nets, extra_nets, unknown_nets, device_diff)
+def compare_circuits(circuit1, circuit2):
+    missing_nets = []
+    extra_nets = []
+    unknown_nets = []
+    device_diff = []
 
-            
-            for circuit1_net in circuit_set[0]:
-                found = False
-                for circuit2_net in circuit_set[i]:
-                    if circuit2_net[0] == circuit1_net[0]:  
-                        found = True
-                        for pin_info in circuit2_net[1]:  
-                            if pin_info not in circuit1_net[1]:  
-                                print(f"Mismatch in {circuit2_net[0]} in Circuit {i + 1} compared to Circuit 1: ", pin_info)
-                        break  
-                if not found:
-                    if circuit1_net[0] != '(no matching net)':
-                        print(f"{circuit1_net[0]} not found in Circuit {i + 1}")
-                    else:
-                        print(f"No matching net found in Circuit {i + 1} for {circuit1_net[0]} in Circuit 1. Details: ", circuit1_net[1])
+    # Check for missing, device difference
+    for circuit1_net in circuit1:
+        if circuit1_net[0] == "(no matching net)":
+            continue  # Skip this net, don't consider it missing
+        for pin_info1 in circuit1_net[1]:
+            found_in_c2 = False
+            for circuit2_net in circuit2:
+                if circuit1_net[0] == circuit2_net[0]:  # Matching net names
+                    for pin_info2 in circuit2_net[1]:
+                        if len(pin_info1) > 1 and len(pin_info2) > 1 and pin_info1[0] == pin_info2[0]:  # Check each pin
+                            found_in_c2 = True
+                            if len(pin_info1) > 2 and len(pin_info2) > 2 and pin_info1[2] != pin_info2[2]:  # Different devices
+                                device_diff.append([circuit1_net[0], pin_info2, pin_info1[2]])  # Add circuit2 info and circuit1 device count to the device_diff list
+                            break
+                if found_in_c2:
+                    break
+            if not found_in_c2:  # If pin_info1 is not in any of circuit2_net
+                missing_nets.append([circuit1_net[0], pin_info1])  # this pin is in circuit1_net but not in the corresponding circuit2_net
+
+    # Check for extra and unknown nets
+    for circuit2_net in circuit2:
+        found_in_c1 = False
+        for circuit1_net in circuit1:
+            if circuit1_net[0] == circuit2_net[0]:  # Matching net names
+                found_in_c1 = True
+                for pin_info2 in circuit2_net[1]:
+                    found_in_c1_net = any(len(pin_info1) > 1 and pin_info1[0] == pin_info2[0] and pin_info1[1] == pin_info2[1] for pin_info1 in circuit1_net[1])  # Check each pin and its connections
+                    if not found_in_c1_net:  # If pin_info2 is not in circuit1_net
+                        unknown_nets.append([circuit2_net[0], pin_info2])  # this pin is in circuit2_net but not in the corresponding circuit1_net
+                break
+        if not found_in_c1:  # If circuit2_net is not in circuit1
+            if circuit2_net[0] != "(no matching net)":  # Ignore placeholder net
+                extra_nets.append(circuit2_net)
+
+    return missing_nets, extra_nets, unknown_nets, device_diff
+
+
+
+
+def print_info(missing_nets, extra_nets, unknown_nets, device_diff):
+    if missing_nets:
+        print("Missing nets:")
+        for net in missing_nets:
+            print(net)
+
+    if extra_nets:
+        print("Extra nets:")
+        for net in extra_nets:
+            print(net)
+
+    if unknown_nets:
+        print("Unknown nets:")
+        for net in unknown_nets:
+            print(net)
+
+    if device_diff:
+        print("Device differences:")
+        for diff in device_diff:
+            print(f"In Circuit 2: Net '{diff[0]}', Device '{diff[1][0]}', Connections '{diff[1][1]}', Count {diff[1][2]}")
+            print(f"In Circuit 1: Count {diff[2]} for the same net and device")
+
 
 badnets = [
       [
         [
           [
-            "Z",
+            "(no matching net)",
             [
-              [ "sky130_fd_pr__pfet_01v8", "1|3", 1 ],
-              [ "sky130_fd_pr__nfet_01v8", "1|3", 1 ]
-            ]
-          ],
-          [
-            "A",
-            [
-              [ "sky130_fd_pr__pfet_01v8", "2", 1 ],
-              [ "sky130_fd_pr__nfet_01v8", "2", 1 ]
-            ]
-          ],
-          [
-            "vdd",
-            [
-              [ "sky130_fd_pr__pfet_01v8", "1|3", 1 ],
-              [ "sky130_fd_pr__pfet_01v8", "4", 1 ]
+              [ "", "", 0 ]
             ]
           ]
         ], [
           [
-            "Z",
+            "gnd",
             [
-              [ "sky130_fd_pr__nfet_01v8", "1|3", 1 ],
-              [ "sky130_fd_pr__pfet_01v8", "1|3", 1 ]
+              [ "sky130_fd_sc_hd__inv_1", "VGND", 6 ],
+              [ "sky130_fd_sc_hd__inv_1", "VNB", 6 ],
+              [ "sky130_fd_sc_hd__inv_1", "VPB", 6 ],
+              [ "sky130_fd_sc_hd__inv_1", "VPWR", 6 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VGND", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VNB", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VPB", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VPWR", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VGND", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VNB", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VPB", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VPWR", 1 ]
+            ]
+          ]
+        ]
+      ],
+      [
+        [
+          [
+            "vdd",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "VPB", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VPWR", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VPB", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VPWR", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "VPB", 6 ],
+              [ "sky130_fd_sc_hd__inv_1", "VPWR", 6 ]
+            ]
+          ],
+          [
+            "gnd",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "VGND", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "VNB", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VGND", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "VNB", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "VGND", 6 ],
+              [ "sky130_fd_sc_hd__inv_1", "VNB", 6 ]
+            ]
+          ]
+        ], [
+          [
+            "(no matching net)",
+            [
+              [ "", "", 0 ]
+            ]
+          ],
+          [
+            "(no matching net)",
+            [
+              [ "", "", 0 ]
+            ]
+          ]
+        ]
+      ],
+      [
+        [
+          [
+            "EN",
+            [
+              [ "sky130_fd_sc_hd__nand2_1", "A", 1 ]
+            ]
+          ],
+          [
+            "SEL",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "S", 1 ]
+            ]
+          ]
+        ], [
+          [
+            "SEL",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "S", 1 ]
+            ]
+          ],
+          [
+            "EN",
+            [
+              [ "sky130_fd_sc_hd__nand2_1", "A", 1 ]
+            ]
+          ]
+        ]
+      ],
+      [
+        [
+          [
+            "N3",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ],
+          [
+            "OUT",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ],
+          [
+            "N0",
+            [
+              [ "sky130_fd_sc_hd__nand2_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
             ]
           ],
           [
             "A",
             [
-              [ "sky130_fd_pr__nfet_01v8", "2", 1 ],
-              [ "sky130_fd_pr__pfet_01v8", "2", 1 ]
+              [ "sky130_fd_sc_hd__mux2_1", "X", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "B", 1 ]
+            ]
+          ],
+          [
+            "N6",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "A1", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ]
+            ]
+          ]
+        ], [
+          [
+            "sky130_fd_sc_hd__inv_1_3/Y",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "A1", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ]
+            ]
+          ],
+          [
+            "sky130_fd_sc_hd__inv_1_1/A",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ]
+            ]
+          ],
+          [
+            "sky130_fd_sc_hd__mux2_1_0/X",
+            [
+              [ "sky130_fd_sc_hd__nand2_1", "B", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "X", 1 ]
+            ]
+          ],
+          [
+            "OUT",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ],
+          [
+            "sky130_fd_sc_hd__inv_1_4/A",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ],
+              [ "sky130_fd_sc_hd__nand2_1", "Y", 1 ]
+            ]
+          ]
+        ]
+      ],
+      [
+        [
+          [
+            "N4",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ],
+          [
+            "N5",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ]
+        ], [
+          [
+            "(no matching net)",
+            [
+              [ "", "", 0 ]
             ]
           ],
           [
@@ -86,15 +291,38 @@ badnets = [
           ]
         ], [
           [
-            "w_0_124#",
+            "sky130_fd_sc_hd__inv_1_2/A",
             [
-              [ "sky130_fd_pr__pfet_01v8", "4", 1 ]
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
             ]
           ],
           [
-            "vdd",
+            "sky130_fd_sc_hd__inv_1_3/A",
             [
-              [ "sky130_fd_pr__pfet_01v8", "1|3", 1 ]
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ]
+        ]
+      ],
+      [
+        [
+          [
+            "N2",
+            [
+              [ "sky130_fd_sc_hd__mux2_1", "A0", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ]
+            ]
+          ]
+        ], [
+          [
+            "sky130_fd_sc_hd__inv_1_5/Y",
+            [
+              [ "sky130_fd_sc_hd__inv_1", "Y", 1 ],
+              [ "sky130_fd_sc_hd__inv_1", "A", 1 ],
+              [ "sky130_fd_sc_hd__mux2_1", "A0", 1 ]
             ]
           ]
         ]
--- script.py
+++ script.py
@@ -4,16 +4,48 @@ def parse_json_file(file_name):
     with open(file_name, 'r') as json_file:
         data = json.load(json_file)
 
-        for item in data:
-            badnets = item.get('badnets', [])
-            badelements = item.get('badelements', [])
-            pins = item.get('pins', [])
-            properties = item.get('properties', [])
+        all_badnets = []
+        all_badelements = []
+        all_pins = []
+        all_properties = []
 
-            
-            print("badnets: ", badnets)
-            print("badelements: ", badelements)
-            print("pins: ", pins)
-            print("properties: ", properties)
-            print("\n---\n") 
-parse_json_file("comp.json")
+        badnets_counter = 0
+        badelements_counter = 0
+        pins_counter = 0
+        properties_counter = 0
+
+        for item in data:
+            if 'badnets' in item:
+                badnets = item['badnets']
+                all_badnets.append(badnets)
+                badnets_counter += 1
+
+            if 'badelements' in item:
+                badelements = item['badelements']
+                all_badelements.append(badelements)
+                badelements_counter += 1
+
+            if 'pins' in item:
+                pins = item['pins']
+                all_pins.append(pins)
+                pins_counter += 1
+
+            if 'properties' in item:
+                properties = item['properties']
+                all_properties.append(properties)
+                properties_counter += 1
+
+       
+
+        return all_badnets, all_badelements, all_pins, all_properties ,badnets_counter ,badelements_counter, pins_counter ,properties_counter
+
+file_name = "comp_tut6e.json"
+all_badnets, all_badelements, all_pins, all_properties,badnets_counter ,badelements_counter, pins_counter ,properties_counter = parse_json_file(file_name)
+print("Badnets Count: ", all_badnets)
+print("Badelements Count: ", badelements_counter)
+print("Pins Count: ", pins_counter)
+print("Properties Count: ", properties_counter)
+print(len(all_badnets))
+a= open("l.txt","w+")
+a.write(str(all_badnets))
+a.close()
